e652b05ab331967bd5574927930dfc1115456dc2
Ronald <rolmacdo@gmail.com>
2021-07-13 16:38:19 +0800

    Loop integration

--- APIGoog.py
+++ APIGoog.py
@@ -32,12 +32,17 @@ def authenticate():
     client_secrets_file = "CLI.json"
 
     # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_console()
-    #global youtube
-    #youtube = googleapiclient.discovery.build(
-        #api_service_name, api_version, credentials=credentials)
+    while True:
+        try:
+            flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+                client_secrets_file, scopes)
+            credentials = flow.run_console()
+            global youtube
+            youtube = googleapiclient.discovery.build(
+                api_service_name, api_version, credentials=credentials)
+            break
+        except:
+            print("Invalid key! Try again.")
     
     print('\n')
 
---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+This simple script is designed to browse thru Youtube and be able to download the video in its highest quality.
+
+For authentication, you will need to put the client secret file (JSON file) in the same folder as the script. This client secret file can be obtained from your Youtube API account. You should first sign-up as a Google Developer to access this.
